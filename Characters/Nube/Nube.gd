d7519280ae66589b8eb69a7ef640e91dfa70ad21
Victoria Vitale <victoria@mural.co>
2018-12-16 17:26:56 -0300

    lel

--- Characters/Nube/Nube.gd
+++ Characters/Nube/Nube.gd
@@ -8,6 +8,7 @@ func _ready():
     # Called when the node is added to the scene for the first time.
     # Initialization here
     floats = true
+    speed = 200
 
 #func _process(delta):
 #    # Called every frame. Delta is time since last frame.
