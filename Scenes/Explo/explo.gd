c508b0399ff09c24a7cc641375d0207d5e3df66b
clrnd <welcometothechango@gmail.com>
2018-12-16 14:45:09 -0300

    explo

---
+++ Scenes/Explo/explo.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    connect("animation_finished", self, "_onAnimationEnded")
+
+func _onAnimationEnded():
+    queue_free()
