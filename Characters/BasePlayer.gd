7e72e799babab9afc0a97acaaa91617b547edcc7
clrnd <welcometothechango@gmail.com>
2018-12-16 13:25:27 -0300

    logics

--- Characters/BasePlayer.gd
+++ Characters/BasePlayer.gd
@@ -18,11 +18,19 @@ var k_up = ""
 var k_right = ""
 var k_left = ""
 var k_attack = ""
-var disabled = true
+
+var attacking = false
 
 func _ready():
-    set_physics_process(true)
+    set_physics_process(false)
+    $spr.connect("animation_finished", self, "_onAnimationEnded")
     #set_process(true)
+
+func _onAnimationEnded():
+    if attacking:
+        attacking = false
+        $attackr/col.disabled = true
+        $attackl/col.disabled = true
     
 func set_p1():
     enable()
@@ -47,33 +55,44 @@ func unmap():
     
 func enable():
     $col.disabled = false
-    disabled = false
+    set_physics_process(true)
+    remove_from_group("playables")
     
     
 func deactivate():
     unmap()
+    set_physics_process(false)
     $col.disabled = true
-    disabled = true
-
 
 func _physics_process(delta):
-    if not disabled:
-        _move(delta)
+    _move(delta)
 
 
 func _move(delta):
-    direction.x = int(Input.is_action_pressed(k_right))-int(Input.is_action_pressed(k_left))
     
-    if direction.y != 0 and swim == false:
-        $spr.animation = "jump"
-    
-    if direction.x != 0 and direction.y == 0 and swim == false and open_door == false:
-        $spr.animation = "move"
-        $spr.playing = true
+    if not attacking:
+        direction.x = int(Input.is_action_pressed(k_right))-\
+                        int(Input.is_action_pressed(k_left))
+        if direction.y != 0:
+            $spr.animation = "jump"
         
-    elif direction.x == 0 and direction.y == 0 and swim == false and open_door == false:
-        $spr.playing = false
-        $spr.animation = "default"
+        if direction.x != 0 and direction.y == 0:
+            $spr.animation = "move"
+            $spr.playing = true
+            
+        elif direction.x == 0 and direction.y == 0:
+            $spr.playing = false
+            $spr.animation = "default"
+    else:
+        var lel
+        if not $spr.flip_h:
+            lel = $attackr
+        else:
+            lel = $attackl
+        for x in lel.get_overlapping_bodies():
+            print(x)
+#            get_tree().get_root().get_node("level1").kill(x)
+        direction.x = 0
     
     if direction.x > 0:
         $spr.flip_h = false
@@ -86,10 +105,20 @@ func _move(delta):
     
     move_and_slide(velocity, Vector2(0, -1))
     
+
+    if Input.is_action_just_pressed(k_attack):
+        $spr.animation = "attack"
+        $spr.playing = true
+        attacking = true
+        if not $spr.flip_h:
+            $attackr/col.disabled = false
+        else:
+            $attackl/col.disabled = false
+    
     if is_on_floor():
         velocity.y = 0
         direction.y = 0
         
-        if Input.is_action_just_pressed(k_up):
+        if Input.is_action_just_pressed(k_up) and not attacking:
             velocity.y = -jump_speed
             direction.y = 1
--- Levels/level1.gd
+++ Levels/level1.gd
@@ -4,20 +4,21 @@ extends Node
 # var a = 2
 # var b = "textvar"
 
-var player1 = "Player"
-var player2 = "Sapo"
-var playables = ["Player", "Sapo", "Tipito"]
+var player2 = "Tipito"
+var player1 = "Sapo"
 
 func _ready():
-    # Called when the node is added to the scene for the first time.
-    # Initialization here
-    $Player.set_p1()
-#    get_node("Sapo").set_p2()
-    #$Sapo.set_p2()
+    $Sapo.set_p1()
+    $Tipito.set_p2()
+    
+func kill(x):
+    print("LEL")
+    x.die()
 
 func _process(delta):
     if Input.is_action_pressed("ui_space"):
         get_node(player1).deactivate()
+        var playables = get_nodes_in_group("playables")
         var node = playables[randi() % playables.size()]
         player1 = node
         get_node(node).set_p1()
