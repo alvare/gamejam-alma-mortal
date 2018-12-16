ccbff3e6291d4438bded5f06d871b1051aad49a9
clrnd <welcometothechango@gmail.com>
2018-12-16 16:09:16 -0300

    logica de float

--- Characters/BasePlayer.gd
+++ Characters/BasePlayer.gd
@@ -4,11 +4,7 @@ var speed = 350
 var jump_speed = 900
 var gravity = 1300
 
-var swim = false
-var cont = 0
-var text_actual = null
-var shield = false
-var open_door = false
+var floats = false
 
 var distance = Vector2()
 var velocity = Vector2()
@@ -16,6 +12,7 @@ var direction = Vector2()
 
 var k_up = ""
 var k_right = ""
+var k_down = ""
 var k_left = ""
 var k_attack = ""
 
@@ -26,29 +23,33 @@ var attacking = false
 func _ready():
     set_physics_process(false)
     $spr.connect("animation_finished", self, "_onAnimationEnded")
-    #set_process(true)
 
 func _onAnimationEnded():
     if attacking:
         attacking = false
         $attackr/col.disabled = true
         $attackl/col.disabled = true
+        if floats:
+            $spr.playing = false
+            $spr.animation = "default"
 
 func set_p1():
     enable()
     is_p1 = true
     k_up = "ui_up"
     k_right = "ui_right"
+    k_down = "ui_down"
     k_left = "ui_left"
-    k_attack = "ui_down"
+    k_attack = "ui_slash"
 
 func set_p2():
     enable()
     is_p1 = false
     k_up = "ui_w"
     k_right = "ui_d"
+    k_down = "ui_s"
     k_left = "ui_a"
-    k_attack = "ui_s"
+    k_attack = "ui_q"
 
 func unmap():
     k_up = ""
@@ -69,7 +70,19 @@ func deactivate():
     $col.disabled = true
 
 func _physics_process(delta):
-    _move(delta)
+    if floats:
+        _float(delta)
+    else:
+        _move(delta)
+
+    if Input.is_action_just_pressed(k_attack):
+        $spr.animation = "attack"
+        $spr.playing = true
+        attacking = true
+        if not $spr.flip_h:
+            $attackr/col.disabled = false
+        else:
+            $attackl/col.disabled = false
 
 
 func _move(delta):
@@ -108,16 +121,6 @@ func _move(delta):
     
     move_and_slide(velocity, Vector2(0, -1))
     
-
-    if Input.is_action_just_pressed(k_attack):
-        $spr.animation = "attack"
-        $spr.playing = true
-        attacking = true
-        if not $spr.flip_h:
-            $attackr/col.disabled = false
-        else:
-            $attackl/col.disabled = false
-    
     if is_on_floor():
         velocity.y = 0
         direction.y = 0
@@ -125,3 +128,32 @@ func _move(delta):
         if Input.is_action_just_pressed(k_up) and not attacking:
             velocity.y = -jump_speed
             direction.y = 1
+            
+func _float(delta):
+    
+    if not attacking:
+        direction.x = int(Input.is_action_pressed(k_right))-\
+                        int(Input.is_action_pressed(k_left))
+        direction.y = int(Input.is_action_pressed(k_down))-\
+                        int(Input.is_action_pressed(k_up))
+    else:
+        var lel
+        if not $spr.flip_h:
+            lel = $attackr
+        else:
+            lel = $attackl
+        for x in lel.get_overlapping_bodies():
+            get_tree().get_root().get_node("level1").kill(x)
+        direction.x = 0
+    
+    if direction.x > 0:
+        $spr.flip_h = false
+    elif direction.x < 0:
+        $spr.flip_h = true
+    
+    distance.x = speed*delta
+    distance.y = speed*delta
+    velocity.x = (direction.x*distance.x)/delta
+    velocity.y = (direction.y*distance.y)/delta
+    
+    move_and_slide(velocity, Vector2(0, 0))
